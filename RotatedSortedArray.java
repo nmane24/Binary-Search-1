7e2b14e6b96124d0beceaa5ae9da4f3a45769065
Niketa Mane <nmane@Niketas-MacBook-Pro.local>
2024-07-15 21:46:42 -0700

    Binary Search Implementations

--- RotatedSortedArray.java
+++ RotatedSortedArray.java
@@ -11,12 +11,25 @@ There is an integer array nums sorted in ascending order (with distinct values).
 Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
 Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
 You must write an algorithm with O(log n) runtime complexity.
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+Example 3:
+
+Input: nums = [1], target = 0
+Output: -1
+
 */
 
 
 /* Code Explanation
  * In a rotated sorted array, everytime one of the part of the array will be sorted. It can happen both the parts are also sorted;
- * but atleast on of the parts will be sorted. We will be using binary seacrh to divide the search space in half and 
+ * but atleast on of the parts will be sorted. We will be using binary search to divide the search space in half and 
  * checking if the array is left sorted or right sorted; and post that if the element lies in which part of the array. 
  * The target is compared with the middle value of search space and every time the mid is moved by checking the range of target.
  * 
--- search_in_unknown_size_array.java
+++ search_in_unknown_size_array.java
@@ -15,6 +15,19 @@ returns 231 - 1 if the i is out of the boundary of the array.
 You are also given an integer target.
 Return the index k of the hidden array where secret[k] == target or return -1 otherwise.
 You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+
+Input: secret = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in secret and its index is 4.
+Example 2:
+
+Input: secret = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in secret so return -1.
+
+
 */
 
 
